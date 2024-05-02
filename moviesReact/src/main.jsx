a2af6456cac64b644b80b7e27660bc6210a62b07
Asteris <94953985+AsterisCrack@users.noreply.github.com>
2024-05-02 13:04:57 +0200

    Added routes for login and register #3 #4

--- moviesReact/src/main.jsx
+++ moviesReact/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import ListPage from './ListPage.jsx'
+import LoginPage from './Login.jsx'
+import RegisterPage from './Register.jsx'
 import ContactInfo from './ContactInfo.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -15,7 +17,14 @@ const router = createBrowserRouter([{
   },{
     path: "contactInfo",
     element: <ContactInfo/>,
-  }],
+  },{
+    path: "login",
+    element: <LoginPage/>,
+  },{
+    path: "register",
+    element: <RegisterPage/>,
+  }
+],
 }]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
