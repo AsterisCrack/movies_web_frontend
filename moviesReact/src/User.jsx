696af543bb302b676b4c9c397903e6f7a11ad4a4
Asteris <94953985+AsterisCrack@users.noreply.github.com>
2024-05-15 19:36:34 +0200

    Updated request for fetching user

--- moviesReact/src/User.jsx
+++ moviesReact/src/User.jsx
@@ -21,6 +21,22 @@ export default function UserInfoPage() {
     const [deleteConfirmationPassword, setDeleteConfirmationPassword] = useState("");
     const [deleteConfirmationError, setDeleteConfirmationError] = useState("");
     const [showDeleteConfirmationError, setShowDeleteConfirmationError] = useState(false);
+    
+    // Get the current user information
+    useEffect(() => {
+        // Fetch user information from http://apps/users/me with session cookie
+        fetch('http://127.0.0.1:8000/apps/users/me/', {
+            method: 'GET',
+            credentials: 'include', // This will include the session cookie in the request
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setUser(data);
+            // Set other state variables if needed
+        })
+        .catch(error => console.error('Error:', error));
+    }, []);
 
     const handleUsernameEdit = () => {
         setEditingUsername(true);
